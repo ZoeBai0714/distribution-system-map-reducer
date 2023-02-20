24accae2886813750dddb4915fe358a795e49287
github username <s.xue.bai@gmail.com>
2023-02-20 17:25:48 -0600

    start to build mapper

--- readMe.md
+++ readMe.md
@@ -4,13 +4,16 @@ How to run
 1. Go to src/main, start the coordinator to listen to worker (func server).  
    go run mrcoordinator.go pg*.txt
 
-2. Open another terminal, go to src/main, start a worker to ask coordinator for a task. 
+2. Build our coordinator (MakeCoordinator) for 1 to call upon. Once it gets called, it will grab the files, and create a task for each file.
+
+3. create a catchTimeout func to check if there's any task gets blocked(more than 10 seconds), if so put it back to the queue to have another worker work on it.
+
+4. Open another terminal, go to src/main, start a worker (can open multiple worker in multiple terminals) to ask coordinator for a task. 
    go build -buildmode=plugin ../mrapps/wc.go
    go run mrworker.go wc.so
 
-3. Build our coordinator (MakeCoordinator) for 1 to call upon. Once it gets called, it will grab the files, and create a task for each file.
-
-4. create a catchTimeout func to check if there's any task gets blocked(more than 10 seconds), if so put it back to the queue to have another worker work on it.
+5. Start to build worker that controls mapper and reducer. Once it gets a task from #4, check which status is the task in and act coordingly.
+   If a task is still in Map status, meaning it hasn't been processed, then we process it. After done we write it to local file.
 
 Workflow (Design)
 
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -3,6 +3,7 @@ package mr
 import (
 	"fmt"
 	"hash/fnv"
+	"io/ioutil"
 	"log"
 	"net/rpc"
 	"os"
@@ -12,18 +13,14 @@ import (
 orders:  Worker() -> askForTask() -> call() -> coordinatorSock() from rpc.go
 */
 
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
@@ -31,16 +28,16 @@ func ihash(key string) int {
 }
 
 
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
 	// Your worker implementation here.
-	// iterate over a channel, receive tasks until run out
     for {
 		task := askForTask()
-		fmt.Println("task...", task)
+		fmt.Println("task: ", task)
 		switch task.TaskState {
+		case Map:
+			// assign to mapper to get the file ready to be processed
+			// continue from here, create mapper function
 		}
 	}
 	// uncomment to send the Example RPC to the coordinator.
@@ -57,11 +54,37 @@ func askForTask() Task {
 	return reply
 }
 
-//
+func mapper(task *Task, mapf func(string, string) []KeyValue) {
+
+	content, err := ioutil.ReadFile(task.Input)
+
+	if err != nil {
+		log.Fatal("Failed to read file: "+task.Input, err)
+	}
+
+	// parse the content to map function that emits an array of key value pair
+	// then store them in buffer,
+	intermediates := mapf(task.Input, string(content))
+	buffer := make([][]KeyValue, task.NReducer)
+	for _, intermediate := range intermediates {
+		slot := ihash(intermediate.Key) % task.NReducer
+		buffer[slot] = append(buffer[slot], intermediate)
+	}
+	mapOutput := make([]string, 0)
+	for i := 0; i < task.NReducer; i++ {
+		mapOutput = append(mapOutput, writeToLocalFile(task.TaskNumber, i, &buffer[i]))
+	}
+}
+
+func writeToLocalFile(taskNumber int, i int, buffer *[]KeyValue) string {
+	return "hi"
+	// continue from here, maybe we don't need to buffer to nReduce, just write intermediates the whole thing to file
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
+
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// client, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
